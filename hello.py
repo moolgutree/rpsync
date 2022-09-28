bbf35eb4632ee12da2da75811432cf6defd96e12
LEJ <brave3633@naver.com>
2022-09-28 14:59:11 +0900

    hello.py 파일 추가

---
+++ hello.py
@@ -0,0 +1,16 @@
+avg = (5476) / 2
+print("Average:", avg)
+if avg >= 70:
+    print("you're a pass!")
+else:
+    print("you're rejected!")
+
+i = input("1이 들어가는 2 이상의 숫자 입력>> ")
+for i in 1:
+    if i < 2:
+        print("잘 했습니다.")
+        break
+    else:
+        print('다시 입력새 주세요.')
+        break
+        
